c484a7e66dfdb101127d938676fecd22e764271a
Stanley Svensson <stanley.svensson@gmail.com>
2013-04-26 19:53:49 +0200

    Added CForm for form handling and created a login-form on user/login

--- src/CCUser/login.tpl.php
+++ src/CCUser/login.tpl.php
@@ -1,6 +1,8 @@
 <h1>Login</h1>
 <hr>
-<p>Here should a login form be, but for now you can login using these links.</p>
+<p>Login using your acronym or email.</p>
+<?=$login_form?>
+<!--
 <ul>
     <li><a href="<?=create_url(null, 'login', 'doe/doe')?>">Login as doe (should work)</a></li>
     <li><a href="<?=create_url(null, 'login', 'root/root')?>">Login as root:root (should work)</a></li>
@@ -10,3 +12,4 @@
     <li><a href="<?=create_url(null, 'login', 'admin@gmail.com/root')?>">Login as admin@gmail.com:root (Shold fail, wrong email)</a></li>
     <li><a href="<?=create_url(null, 'logout')?>">Logout</a></li>
 </ul>
+-->
